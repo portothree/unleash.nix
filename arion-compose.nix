20b30e4564f40ccd44e172957e061c62dbe9ea34
Gustavo Porto <gustavo@portosanti.com>
2022-08-12 19:22:05 +0100

    fix: unleash docker image repo

--- arion-compose.nix
+++ arion-compose.nix
@@ -2,7 +2,7 @@
   config.services = {
     unleash-server = {
       service = {
-        image = "unleash/unleash-server:main-edge-16-alpine";
+        image = "unleashorg/unleash-server:main-edge-16-alpine";
         container_name = "unleash-server";
         ports = [ "4242:4242" ];
         stop_signal = "SIGINT";
