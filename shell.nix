787fd679e2466adca1525835ecc48387cf00aca0
Gustavo Porto <gustavo@portosanti.com>
2022-08-18 12:16:11 +0100

    chore: use nodejs v18

--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell { buildInputs = with pkgs; [ nodejs-16_x arion lazydocker ]; }
+pkgs.mkShell { buildInputs = with pkgs; [ nodejs-18_x arion lazydocker ]; }
